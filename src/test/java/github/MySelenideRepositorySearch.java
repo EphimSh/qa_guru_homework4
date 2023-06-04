0b26fdf0d8e1bc18df7ab9459c92874612691645
EphimSh <ephimizm@gmail.com>
2023-06-04 21:10:11 +0900

    MySelenideRepositorySearch updated

--- src/test/java/github/MySelenideRepositorySearch.java
+++ src/test/java/github/MySelenideRepositorySearch.java
@@ -19,6 +19,7 @@ public class MySelenideRepositorySearch {
 
     @Test
     void selenideWikiSoftAssertionsPageShouldHaveJUnit5CodeExamples() {
+        //changes
         open("https://github.com");
 
         $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
